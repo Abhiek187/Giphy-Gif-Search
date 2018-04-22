21788fe9db6f9ee0daacda57177aba520820c24e
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2018-04-21 20:30:09 -0400

    Add home page

--- app.js
+++ app.js
@@ -21,3 +21,7 @@ app.get('/greetings/:name', function (req, res) {
   var name = req.params.name;
   res.render('greetings', {name: name});
 })
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
