6753c38140a6ffbb99dd4f803d1a78a30b905bbf
Abhiek187 <achaudhuri2011@yahoo.com>
2021-11-13 16:43:10 -0500

    Deploy for heroku

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
 // INITIALIZE THE GIPHY-API LIBRARY
-var giphy = require('giphy-api')();
+const giphy = require("giphy-api")();
 
-app.get('/hello-gif', function (req, res) {
-	var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
-	res.render('hello-gif', {gifUrl: gifUrl});
-})
-
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.get("/hello-gif", function (req, res) {
+    const gifUrl = "http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif";
+    res.render("hello-gif", { gifUrl: gifUrl });
 });
 
-var exphbs  = require('express-handlebars');
+const port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
 
-app.get('/greetings/:name', function (req, res) {
-	var name = req.params.name;
-	res.render('greetings', {name: name});
-})
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/greetings/:name", function (req, res) {
+    const name = req.params.name;
+    res.render("greetings", { name: name });
+});
 
 // REQUIRE HTTP MODULE
-var http = require('http');
+const http = require("http");
 
-app.get('/', function (req, res) {
-	if (req.query.term == null || req.query.term == "") {
-		giphy.trending(function (err, response) {
-			res.render('home', {gifs: response.data});
-		});
-	} else {
-		giphy.search(req.query.term, function (err, response) {
-			res.render('home', {gifs: response.data});
-		});
-	}
+app.get("/", function (req, res) {
+    if (req.query.term === null || req.query.term === "") {
+        giphy.trending(function (err, response) {
+            res.render("home", { gifs: response.data });
+        });
+    } else {
+        giphy.search(req.query.term, function (err, response) {
+            res.render("home", { gifs: response.data });
+        });
+    }
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
